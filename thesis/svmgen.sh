283565486fadc93e8e7250a0b711518ba9f9f659
Kien Truong <duckientruong@gmail.com>
2016-02-10 14:41:59 +0100

    Add SvmGen
    Data generator for SVM

---
+++ thesis/svmgen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+spark-submit --class pt.tecnico.spark.svm.SvmGen \
+    target/scala-2.10/thesis_2.10-1.6.0.jar \
+    /home/dikei/Tools/tmp/spark-testing/data/svm \
+    10000000 \
+    10 \
+    4
