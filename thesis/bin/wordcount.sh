21403a816cfde961e6093f1bedaba4a11e94bb6f
Kien Truong <duckientruong@gmail.com>
2016-03-07 11:09:51 +0000

    Format script

--- thesis/bin/wordcount.sh
+++ thesis/bin/wordcount.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-spark-submit --class pt.tecnico.spark.WordCount target/scala-2.10/thesis_2.10-1.6.0.jar /home/dikei/Tools/tmp/spark-testing/data/pride.txt /home/dikei/Tools/tmp/spark-testing/out/pride
+spark-submit --class pt.tecnico.spark.WordCount \
+    target/scala-2.10/thesis_2.10-1.6.0.jar \
+    /home/dikei/Tools/tmp/spark-testing/data/pride.txt \
+    /home/dikei/Tools/tmp/spark-testing/out/pride
